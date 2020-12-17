0119753faacc98286c2d04eeb73725817d58dc04
ffpy <q1411603774@163.com>
2020-12-17 17:25:28 +0800

    管理接口添加context-path

--- src/main/java/com/tom/common/localcache/controller/ManagerController.java
+++ src/main/java/com/tom/common/localcache/controller/ManagerController.java
@@ -3,6 +3,7 @@ package com.tom.common.localcache.controller;
 import com.tom.common.localcache.constant.PathConstant;
 import com.tom.common.localcache.properties.LocalCacheManagerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +20,12 @@ public class ManagerController {
     @Autowired
     private LocalCacheManagerProperties localCacheManagerProperties;
 
+    @Value("${server.servlet.context-path:}")
+    private String contextPath;
+
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("path", localCacheManagerProperties.getPath());
+        model.addAttribute("path", contextPath + localCacheManagerProperties.getPath());
         model.addAttribute("name", localCacheManagerProperties.getName());
         return "local-cache/index";
     }
